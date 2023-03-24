465f1cb2a99f1c4644adf210e2ac4ccfeb394582
Onur Eren Hanife <onurhnf@gmail.com>
2023-03-24 03:13:37 +0300

    Added some Comments

--- Case1/app.js
+++ Case1/app.js
@@ -1,5 +1,8 @@
 import getUserData from "./getUserData.js";
 
+// Call the getUserData
+// Resolve Promise and log the returned data
+// Catch any errors that may occur and log them
 getUserData(1)
   .then((data) => console.log(data))
   .catch((error) => console.error(error));
--- Case1/getUserData.js
+++ Case1/getUserData.js
@@ -1,11 +1,14 @@
 import axios from "axios";
 
+// An async function that takes a single parameter userId
 async function getUserData(userId) {
+  // Use Promise.all to make concurrent requests to the JSONPlaceholder API for user and post data
   const [userData, postsData] = await Promise.all([
     axios.get(`https://jsonplaceholder.typicode.com/users/${userId}`),
     axios.get(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`),
   ]);
 
+  // Spread the returned user and post data into a single object and return it
   return {
     ...userData.data,
     posts: postsData.data,
